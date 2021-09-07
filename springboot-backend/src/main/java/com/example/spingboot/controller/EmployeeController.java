11d8e75fe2869ed4b42a8220df466214b0d8626c
Nipun Bahri <bahri.nipun@gmail.com>
2021-09-07 17:43:10 +0200

    Added actuator & swagger api documentation

--- springboot-backend/src/main/java/com/example/spingboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/example/spingboot/controller/EmployeeController.java
@@ -3,9 +3,11 @@ package com.example.spingboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.spingboot.model.Employee;
@@ -19,8 +21,8 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	// Get All Employees
 	@GetMapping(path = "/employees")
+	@ResponseStatus(code = HttpStatus.OK)
 	public List<Employee> getEmployees(){
 		return employeeRepository.findAll();
 	}
