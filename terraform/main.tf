a1a9c5250b32e2bd97401c54ba288a724b7c06d3
Nipun Bahri <bahri.nipun@gmail.com>
2021-09-23 12:45:51 +0200

    rg

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,12 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = "~>2.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
@@ -17,22 +14,4 @@ provider "azurerm" {
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "tfaks9"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "dev"
-  }
-}
-
-resource "azurerm_storage_container" "example" {
-  name                  = "akstfstate"
-  storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
 }
