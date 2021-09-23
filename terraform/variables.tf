b943167b7c02372fb2a14186704d60854d61589b
Nipun Bahri <bahri.nipun@gmail.com>
2021-09-23 13:32:12 +0200

    k8 tf iac

---
+++ terraform/variables.tf
@@ -0,0 +1,42 @@
+variable "client_id" {
+    default = "468b6d82-4d1f-4752-947e-b61d196b40fc"
+}
+variable "client_secret" {
+    default = "uAtl_hpxnR.M4G5vdzYqd01poGv9.zW_lv"
+}
+
+variable "agent_count" {
+    default = 1
+}
+
+variable "ssh_public_key" {
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+    default = "k8stest"
+}
+
+variable cluster_name {
+    default = "k8stest"
+}
+
+variable resource_group_name {
+    default = "azure-k8stest"
+}
+
+variable location {
+    default = "West Europe"
+}
+
+variable log_analytics_workspace_name {
+    default = "testLogAnalyticsWorkspaceName"
+}
+
+variable log_analytics_workspace_location {
+    default = "westeurope"
+}
+
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
+}
