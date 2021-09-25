1af6a84e1d11ef8da8f911b7a661214ef5f6fcc6
Nipun Bahri <bahri.nipun@gmail.com>
2021-09-25 22:09:57 +0200

    asas

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,5 +103,5 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_role_assignment" "aks_sp_container_registry" {
     scope                = azurerm_container_registry.acr.id
     role_definition_name = "AcrPull"
-    principal_id         = azurerm_kubernetes_cluster.k8s.identity[0].object_id
+    principal_id         = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
 }
