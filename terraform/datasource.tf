112d5e116821166e933986a07ab66b683a3fb79e
Nipun Bahri <bahri.nipun@gmail.com>
2021-10-01 22:05:33 +0200

    data

--- terraform/datasource.tf
+++ terraform/datasource.tf
@@ -1,24 +1,24 @@
 ### Key Vault #######################################
-data "azurerm_key_vault" "terraform_vault" {
-  name                = var.keyvault_name
-  resource_group_name = var.keyvault_rg
-}
+# data "azurerm_key_vault" "terraform_vault" {
+#   name                = var.keyvault_name
+#   resource_group_name = var.keyvault_rg
+# }
 
-data "azurerm_key_vault_secret" "ssh_public_key" {
-  name         = "LinuxSSHPubKey"
-  key_vault_id = data.azurerm_key_vault.terraform_vault.id
-}
+# data "azurerm_key_vault_secret" "ssh_public_key" {
+#   name         = "LinuxSSHPubKey"
+#   key_vault_id = data.azurerm_key_vault.terraform_vault.id
+# }
 
-data "azurerm_key_vault_secret" "spn_id" {
-  name         = "spn-id"
-  key_vault_id = data.azurerm_key_vault.terraform_vault.id
-}
-data "azurerm_key_vault_secret" "spn_secret" {
-  name         = "spn-secret"
-  key_vault_id = data.azurerm_key_vault.terraform_vault.id
-}
+# data "azurerm_key_vault_secret" "spn_id" {
+#   name         = "spn-id"
+#   key_vault_id = data.azurerm_key_vault.terraform_vault.id
+# }
+# data "azurerm_key_vault_secret" "spn_secret" {
+#   name         = "spn-secret"
+#   key_vault_id = data.azurerm_key_vault.terraform_vault.id
+# }
 
-### Azure AD #######################################
-data "azuread_service_principal" "aks_principal" {
-  application_id = data.azurerm_key_vault_secret.spn_id.value
-}
+# ### Azure AD #######################################
+# data "azuread_service_principal" "aks_principal" {
+#   application_id = data.azurerm_key_vault_secret.spn_id.value
+# }
