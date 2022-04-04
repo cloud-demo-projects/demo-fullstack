cac80f7e0ff7912d41c8b2aec5e3b6d2f73393b0
Nipun Bahri <bahri.nipun@gmail.com>
2022-04-04 16:33:25 +0200

    fdf

--- test-script.sh
+++ test-script.sh
@@ -1,1 +1,6 @@
 echo ${testarg1}
+
+az devops configure --defaults organization=https://dev.azure.com/bahrinipun project=demo-fullstack
+echo $(PAT_TOKEN) | az devops login --organization https://dev.azure.com/bahrinipun
+
+az devops project list --detect
