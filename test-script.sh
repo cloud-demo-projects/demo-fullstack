9aedbc9c852666ccb3c4a05322d82c5ee18fc411
Nipun Bahri <bahri.nipun@gmail.com>
2022-04-04 16:54:20 +0200

    dsds

--- test-script.sh
+++ test-script.sh
@@ -1,8 +1,10 @@
 echo "2- ${PAT_TOKEN}"
 echo "3- ${VAR1}"
 echo "4- ${Sytem.CollectionUri}"
+echo "5- ${VAR2}"
+echo "6- ${{ parameters.PARAM }}"
 
 az devops configure --defaults organization=https://dev.azure.com/bahrinipun project=demo-fullstack
 echo ${PAT_TOKEN} | az devops login --organization https://dev.azure.com/bahrinipun
 
-az devops project list --org https://dev.azure.com/bahrinipun
+#az devops project list --org https://dev.azure.com/bahrinipun
