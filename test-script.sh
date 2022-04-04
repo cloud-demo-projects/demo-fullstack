f9c4347b992542f2ba0e83c079ccd94e6565c472
Nipun Bahri <bahri.nipun@gmail.com>
2022-04-04 16:41:33 +0200

    sfdf

--- test-script.sh
+++ test-script.sh
@@ -1,4 +1,5 @@
 echo "2- ${PAT_TOKEN}"
+echo "3- ${var1}"
 
 az devops configure --defaults organization=https://dev.azure.com/bahrinipun project=demo-fullstack
 echo ${PAT_TOKEN} | az devops login --organization https://dev.azure.com/bahrinipun
