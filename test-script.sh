5077762c1929705d000b1aa4f1a1d0b2d173714b
Nipun Bahri <bahri.nipun@gmail.com>
2022-04-04 17:03:09 +0200

    sfsf

--- test-script.sh
+++ test-script.sh
@@ -3,6 +3,7 @@ echo "3- ${VAR1}"
 echo "4- ${Sytem.CollectionUri}"
 echo "5- ${VAR2}"
 echo "6- ${{ parameters.PARAM }}"
+echo "7- ${TESTURL}"
 
 az devops configure --defaults organization=https://dev.azure.com/bahrinipun project=demo-fullstack
 echo ${PAT_TOKEN} | az devops login --organization https://dev.azure.com/bahrinipun
