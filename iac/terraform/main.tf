62188cdf275a87f3b1c816bd1e2db668ff49a117
Nipun Bahri <bahri.nipun@gmail.com>
2022-05-17 15:43:47 +0200

    delete

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_log_analytics_solution" "fullstack_lws_solution" {
     }
 }
 
-resource "azurerm_application_insights" "fullstack_appinsights" {
+resource "azurerm_application_insights" "fullstack-appinsights" {
   name                = "fullstack-appinsights"
   location            = azurerm_resource_group.k8s.location
   resource_group_name = azurerm_resource_group.k8s.name
