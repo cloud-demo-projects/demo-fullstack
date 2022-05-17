aed6f7b818b94f7c78a0356a613222a8b27dd806
Nipun Bahri <bahri.nipun@gmail.com>
2022-05-17 15:44:48 +0200

    renamed

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_log_analytics_solution" "fullstack_lws_solution" {
     }
 }
 
-resource "azurerm_application_insights" "fullstack-appinsights" {
+resource "azurerm_application_insights" "applicationinsights" {
   name                = "fullstack-appinsights"
   location            = azurerm_resource_group.k8s.location
   resource_group_name = azurerm_resource_group.k8s.name
