73e780f6d86f13c76bec47e0266b0abfa5ad8149
Nipun Bahri <bahri.nipun@gmail.com>
2022-05-20 21:28:26 +0200

    updates

--- springboot-backend/src/main/java/com/example/spingboot/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/com/example/spingboot/SpringbootBackendApplication.java
@@ -1,9 +1,11 @@
 package com.example.spingboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,ManagementWebSecurityAutoConfiguration.class })
 public class SpringbootBackendApplication {
 
 	public static void main(String[] args) {
