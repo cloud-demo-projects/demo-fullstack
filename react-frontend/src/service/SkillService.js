368bb6206fb727b212fa5940c3e0c66d4f40bcaf
Nipun Bahri <bahri.nipun@gmail.com>
2022-05-21 21:41:11 +0200

    ssl disable

--- react-frontend/src/service/SkillService.js
+++ react-frontend/src/service/SkillService.js
@@ -1,12 +1,18 @@
 import axios from "axios";
+import https from 'https';
 
 class SkillService {
 
     getSkills() {
+        const agent = new https.Agent({  
+            rejectUnauthorized: false
+        });
+
         console.log(process.env.REACT_APP_SKILL_API_BASE_URL);
         return axios.request({method: "GET",
                               url: process.env.REACT_APP_SKILL_API_BASE_URL,
-                              crossDomain: true})
+                              crossDomain: true,
+                              httpAgent: agent})
     }
 
 }
