58d69ccc2a97da04bfe38eb8efab4badf5a92ec2
Nipun Bahri <bahri.nipun@gmail.com>
2022-05-21 21:52:19 +0200

    updates

--- react-frontend/src/service/SkillService.js
+++ react-frontend/src/service/SkillService.js
@@ -1,18 +1,16 @@
 import axios from "axios";
-import https from 'https';
 
 class SkillService {
 
     getSkills() {
-        const agent = new https.Agent({  
-            rejectUnauthorized: false
-        });
+        // const agent = new https.Agent({  
+        //     rejectUnauthorized: false
+        // });
 
         console.log(process.env.REACT_APP_SKILL_API_BASE_URL);
         return axios.request({method: "GET",
                               url: process.env.REACT_APP_SKILL_API_BASE_URL,
-                              crossDomain: true,
-                              httpAgent: agent})
+                              crossDomain: true})
     }
 
 }
