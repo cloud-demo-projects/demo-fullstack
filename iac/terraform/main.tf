36030c340c7b9b0999bc2c513c2d6c1f16822443
Nipun Bahri <bahri.nipun@gmail.com>
2022-05-26 16:11:56 +0200

    MIO role

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -202,12 +202,12 @@ resource "azurerm_user_assigned_identity" "this" {
   resource_group_name = azurerm_resource_group.k8s.name
 }
 
-# resource "azurerm_role_assignment" "managed_identity_operator" {
-#   principal_id                     = azurerm_user_assigned_identity.this[0].object_id
-#   scope                            = azurerm_user_assigned_identity.this.id
-#   role_definition_name             = "Managed Identity Operator"
-#   skip_service_principal_aad_check = true
-# }
+resource "azurerm_role_assignment" "managed_identity_operator" {
+  principal_id                     = azurerm_user_assigned_identity.this.client_id
+  scope                            = azurerm_user_assigned_identity.this.id
+  role_definition_name             = "Managed Identity Operator"
+  skip_service_principal_aad_check = true
+}
 
 resource "azurerm_role_assignment" "keyvault_reader" {
   principal_id                     = azurerm_user_assigned_identity.this.client_id
