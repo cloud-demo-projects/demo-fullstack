d6e15073dd60f4c678b35494c4dab37777c96b7b
Nipun Bahri <bahri.nipun@gmail.com>
2022-06-15 15:33:22 +0200

    versions moved

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,15 +1,15 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = "~>2.0"
+#     }
+#   }
+# }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 
 ############ Resource Group ##############
 resource "azurerm_resource_group" "k8s" {
